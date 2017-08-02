c8fe16c7660044c69ce0bbe823d7b6e56c5d2900
neil <neil.zheng@searching-info.com>
2017-08-02 14:28:18 +0800

    RN0.47

--- android/src/main/java/com/reactlibrary/sm_alert/SMOSmAlertPackage.java
+++ android/src/main/java/com/reactlibrary/sm_alert/SMOSmAlertPackage.java
@@ -16,7 +16,7 @@ public class SMOSmAlertPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new SMOSmAlertModule(reactContext));
     }
 
-    @Override
+//    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
